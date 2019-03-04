e8809b25016dfafca3fcdd8bb426e358fe867ed8
dylanOshima <dylanoshima@gmail.com>
2019-03-04 00:26:03 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const FileLoader = require("file-loader")
+
+module.exports = {
+  // output: {
+  //
+  // },
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: "babel-loader"
+      //   }
+      // },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: "./index.html",
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./src/main.css",
+      chunkFilename: "1.css"
+    })
+  ]
+};
